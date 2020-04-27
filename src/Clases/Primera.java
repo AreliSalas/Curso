736889ab25655e2bd6d605cd4e5566a55a4285d3
Areli <sonia_are@live.com>
2020-04-26 20:03:14 -0500

    Carga de vesion 2

--- src/Clases/Primera.java
+++ src/Clases/Primera.java
@@ -3,6 +3,12 @@ package Clases;
 public class Primera {
     public static void main (String [] args){
         String saludo = "Hola Mundo";
+        int num1= 5;
+        int num2= 10;
+        int resultado;
+
+        resultado=num1+num2;
         System.out.println(saludo);
+        System.out.println("El resultado de la suma es igual a: " + resultado);
     }
 }
